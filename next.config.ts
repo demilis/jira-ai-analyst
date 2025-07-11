e4e47ee2fd8472081b50433c5bfe2f558cfdf310
유니아빠 (데미안76) <JeongGyuCheol@gmail.com>
2025-07-11 08:07:31 +0000

    http://jira.lge.com/issue/browse/HMCCCICEX-29637

--- next.config.ts
+++ next.config.ts
@@ -25,14 +25,14 @@ const nextConfig: NextConfig = {
         // 중요: 아래 destination 값을 당신의 실제 Jira 서버 주소로 반드시 변경해야 합니다.
         // --- 404 Not Found 오류가 발생하나요? ---
         // 많은 Jira 서버는 'http://서버주소/jira' 와 같이 하위 경로에 설치됩니다.
-        // 브라우저에서 접속하는 주소를 확인하고, 만약 '/jira'와 같은 경로가 있다면
+        // 브라우저에서 접속하는 주소를 확인하고, 만약 '/issue'와 같은 경로가 있다면
         // destination 값에도 반드시 포함시켜야 합니다.
         //
         // --- 예시 ---
         // destination: 'https://my-jira.my-company.com:8080/jira/:path*',
         // destination: 'http://192.168.1.100/jira/:path*',
         // destination: 'https://your-team.atlassian.net/:path*',
-        destination: 'http://jira.lge.com/:path*', 
+        destination: 'http://jira.lge.com/issue/:path*', 
       },
     ]
   },
